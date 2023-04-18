c662023d73944914f364e01405896f8582b8f0a7
Julio Cesar de Souza <julios100305@gmail.com>
2023-04-18 15:28:01 -0300

    Mysql password update

--- BankWebApp/Program.cs
+++ BankWebApp/Program.cs
@@ -13,7 +13,7 @@ namespace BankWebApp
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddDbContext<bankdataContext>(options => options.UseMySql("server=localhost;port=3306;user=root;password=10032005Jc%;database=bankdata", ServerVersion.Parse("8.0.30-mysql"))
+            builder.Services.AddDbContext<bankdataContext>(options => options.UseMySql("server=localhost;port=3306;user=root;password=1234;database=bankdata", ServerVersion.Parse("8.0.30-mysql"))
                 );
             // Add services to the container.
             builder.Services.AddControllersWithViews();
